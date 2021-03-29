f1405b26aaa2d190575dd3973f4fb49ef6a81d77
cagdas <cagdas.camlibelen@remondis.com.tr>
2021-03-29 21:29:29 +0300

    NavbarBranch içine navbar komutları yazıldı

---
+++ Navbarr.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+ function navbar() {
+     const {title}=this.props;
+    return (
+        <div>
+        <ul>
+            <li><h1>{title}</h1></li>
+            <li><h1>{title}</h1></li>
+        </ul>
+            
+        </div>
+    )
+}
+
+navbar.defaultProps={
+    title:"Bilgi yok"
+}
+navbar.prototype ={
+    title:PropTypes.string.isRequired
+}
+export default navbar;
+
